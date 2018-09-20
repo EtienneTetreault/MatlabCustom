76292e718649dc859656add0001502b1203c3486
EtienneTetreault <etienne.tetreault@gmail.com>
2018-09-20 10:47:00 -0400

    New funciton te generate specific ulog name to send to ulogExtract

---
+++ ulogNumberSelec.m
@@ -0,0 +1,17 @@
+function logName = ulogNumberSelec(logNumberWanted)
+
+logStruct = struct2cell(dir ('Log*.ulg'));
+NameStr = logStruct(1,:).';
+NameStr = sortrows(NameStr);
+
+counter = 0;
+logName = cell(length(logNumberWanted),1);
+for i = 1:length(NameStr)
+    strToCheck = NameStr{i};
+    logNumber = regexp(strToCheck,'(?<=_)\d+(?=_)','match')
+    ToKeep = any(ismember(logNumberWanted,str2double(logNumber{1})));
+    if ToKeep
+        counter = counter +1;
+        logName{counter,1} = NameStr{i}(1:end-4);
+    end
+end
